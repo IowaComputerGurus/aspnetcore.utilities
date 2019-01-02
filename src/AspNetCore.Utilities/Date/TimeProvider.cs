1f15f51ec3081603665446ae053edee7839fa080
Mitchel Sellers <msellers@iowacomputergurus.com>
2019-01-02 03:06:22 -0600

    Initial project commit.

---
+++ src/AspNetCore.Utilities/Date/TimeProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AspNetCore.Utilities.Date
+{
+    public interface ITimeProvider
+    {
+        DateTime CurrentServerTime { get; }
+        DateTime CurrentUtcTime { get; }
+    }
+
+    public class TimeProvider : ITimeProvider
+    {
+        public DateTime CurrentServerTime => DateTime.Now;
+        public DateTime CurrentUtcTime => DateTime.UtcNow;
+    }
+}
